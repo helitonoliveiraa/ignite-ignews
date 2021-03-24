e546e40e366f4a15617ae7e19a1142c4548d22dd
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-24 18:16:39 -0300

    :sparkles: feat: subscribe and integrate with payment stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { useSession, signIn } from 'next-auth/client';
+import { getStripeJs } from '../../services/stripe-js';
+
+import { api } from '../../services/api';
+
 import styles from './styles.module.scss';
 
 type SubscribeButtonProps = {
@@ -5,8 +10,33 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn();
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    } 
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
