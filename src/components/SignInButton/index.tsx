ad26760c9ac0d2972da97d47585e85eee2b7a918
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-23 21:46:30 -0300

    :sparkles: feat: sign in with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,30 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session, loading] = useSession();
 
-  const colorButton = isUserLoggedIn ? "#04D361" : "#EBA417";
+  console.log(session)
 
-  return (
-    <button className={styles.signInButton}>
-      <FaGithub color={colorButton} />
-
-      {isUserLoggedIn ? 'Héliton Oliveira' : 'Sign in with GitHub'}
-      
-      {isUserLoggedIn && <FiX className={styles.closeIcon} />}
+  return session ? (
+    <button 
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04D361" />
+      {session.user.name}
+      <FiX className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#EBA417" />
+      Sitn in with GitHub
     </button>
   )
 }
