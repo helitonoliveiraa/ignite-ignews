2d623e104eef56566f83ad5b92e5366e2e3cc4fc
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-26 17:04:48 -0300

    :sparkles: feat: list post page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>23 de junlho de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+          <a href="#">
+            <time>23 de junlho de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+          <a href="#">
+            <time>23 de junlho de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
