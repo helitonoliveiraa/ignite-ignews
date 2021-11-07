b2fb10153c7aba38bd9f5991c92af9db32c455db
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-07 20:35:48 -0300

    :white_check_mark: test: component ActiveLink

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+
+import { ActiveLink } from './index';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/',
+      }
+    }
+  }
+});
+
+describe('ActiveLink component', () => {
+  it('renders correctly', () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+  
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+  
+  it('adds active class if the link as currently active', () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+  
+    expect(screen.getByText('Home')).toHaveClass('active');
+  });
+});
+
