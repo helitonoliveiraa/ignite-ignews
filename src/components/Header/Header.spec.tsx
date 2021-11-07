d00982f4a13d7ffbd768fdc77fa11ade8969490e
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-07 20:36:15 -0300

    :white_check_mark: test: component Header

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { Header } from './index';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/',
+      }
+    }
+  }
+});
+
+jest.mock('next-auth/client', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+});
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+    render(<Header />);
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+});
