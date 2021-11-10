d11d5b44609cef879567e9f76c74bc7d72e58e21
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-10 20:54:11 -0300

    :wrench: setup coverage report

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,11 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(scss|sass|css)$": "identity-obj-proxy"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+  ],
+  coverageReporters: ["lcov", "json"]
 }
